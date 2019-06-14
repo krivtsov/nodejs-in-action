ce1573ab64891fc82e3c6b9f95a6b12873980b8a
krivtsov <krivtsov.su@gmail.com>
2019-06-15 00:33:18 +0400

    add simple server

---
+++ blog_recent.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('./titles.json', (err1, dataJson) => {
+      if (err1) {
+        console.error(err1);
+        res.end('Server Error');
+      } else {
+        const titles = JSON.parse(dataJson.toString());
+        fs.readFile('./template.html', (err2, dataHtml) => {
+          if (err2) {
+            console.error(err2);
+            res.end('Server Error');
+          } else {
+            const tmpl = dataHtml.toString();
+            const html = tmpl.replace('%', titles.join('</li><li>'));
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(html);
+          }
+        });
+      }
+    });
+  }
+}).listen(8000, '127.0.0.1');
